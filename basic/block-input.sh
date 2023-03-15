d15fd5558ce83057c660a810ff1d230bf1ba7671
Keenan Van Deusen <keenanvandeusen@gmail.com>
2023-03-15 16:41:22 -0400

    block input devices script

---
+++ basic/block-input.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+date >> /home/usbbp/devices.log && echo -e "Start\n" >> /home/usbbp/devices.log
+
+while true; do
+	events=$(ls /dev/input)
+	substring="event"
+	if [[ $events = *"$substring"* ]]; then
+		date >> /home/usbbp/devices.log && echo -e "Device found\n" >> /home/usbbp/devices.log
+		sudo modprobe -r usbhid
+		date >> /home/usbbp/devices.log && echo -e "Device disabled\n" >> /home/usbbp/devices.log
+	fi
+done
