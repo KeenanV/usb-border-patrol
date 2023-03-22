07c1497df150b672745d47e1754efe1cc53fcc4d
jessquintanilla <98177563+jessquintanilla@users.noreply.github.com>
2023-03-22 16:49:01 -0400

    Update unpack.py

--- basic/unpack.py
+++ basic/unpack.py
@@ -11,8 +11,8 @@ class unpack():
     def __init__(self, usb_path):
        self.usb_path = usb_path.usb
 
-    def remove_file(self, path, filename, msg, zip):
-        self.log.append([filename, msg])
+    def remove_file(self, path, filename, file_info, msg, zip):
+        self.log.append([file_info, msg])
         os.remove(self.usb_path + "/" + filename) # delete the extracted file
         if zip:
             cmd=['zip', '-d', path] + [filename] # delete the file in the zip
@@ -27,17 +27,13 @@ class unpack():
             zObject.extractall(path=self.usb_path) # extract all the files
 
             for zfile in unzipped:
+                if os.path.isdir(self.usb_path + "/" + zfile.filename): # if it's a directory
+                    continue 
                 if magic.from_file(self.usb_path + "/" + zfile.filename, mime=True) == "application/zip": # recursive zips
-                    if not self.check_bomb(self.usb_path + "/" + zfile.filename, 1, []):
-                        self.log.append([zfile, "Potential zip bomb detected"])
-                        os.remove(self.usb_path + "/" + zfile.filename) # delete the extracted file
-                        cmd=['zip', '-d', path] + [zfile.filename] # delete the file in the zip
-                        subprocess.check_call(cmd) 
-                elif zfile.file_size > 10000000: # if the zip is greater than a certain size drop it (potential bomb)
-                    self.log.append([zfile, "Zipfile suspiciously large"])
-                    os.remove(self.usb_path + "/" + zfile.filename) # delete the extracted file
-                    cmd=['zip', '-d', path] + [zfile.filename] # delete the file in the zip
-                    subprocess.check_call(cmd)
+                    if zfile.file_size > 10000000: # if the zip is greater than a certain size drop it (potential bomb)
+                        self.remove_file(path, zfile.filename, zfile, "Zipfile suspiciously large", True)
+                    elif not self.check_bomb(self.usb_path + "/" + zfile.filename, 1, []):
+                        self.remove_file(path, zfile.filename, zfile, "Potential zip bomb detected", True)
     
     # unzip it and read the rest of the files
     def tar(self, path):
@@ -47,12 +43,12 @@ class unpack():
         
         for tfile in unzipped:
             if os.path.isdir(self.usb_path + "/" + tfile.name): # if it's a directory
-                continue # self.dir(tfile.name)
+                continue 
             elif tarfile.is_tarfile(self.usb_path + "/" + tfile.name):
-                if tfile.size > 1000000: # if the tar is greater than a certain size drop it (potential bomb)\
-                    self.remove_file(path, tfile.name, "Tarfile suspiciously large", False)
+                if tfile.size > 1000000: # if the tar is greater than a certain size drop it (potential bomb)
+                    self.remove_file(path, tfile.name, tfile, "Tarfile suspiciously large", False)
                 elif not self.check_tbomb(self.usb_path + "/" + tfile.name, 1, []): # recursive tars
-                    self.remove_file(path, tfile.name, "Potential tar bomb detected", False)
+                    self.remove_file(path, tfile.name, tfile, "Potential tar bomb detected", False)
 
     # go through a zip file to check for zip bombs
     def check_bomb(self, path, count, zips):
@@ -97,12 +93,11 @@ class unpack():
                         self.zip(filename) # unzip it
                     elif tarfile.is_tarfile(filename):
                         self.tar(filename) # unzip it
-                # elif not a directory: is irregular file
                     
         log_file = "unpack.log"
         f = open(self.usb_path + "/" + log_file, "w")
         for log in self.log:
-           f.write(str(log[0]) + ": " + log[1] + "\n")
+                f.write(str(log[0]) + ": " + log[1] + "\n\n")
         f.close()
         return
         
