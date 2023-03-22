36da7100f265e1502500df8f98156b6def729410
Kraussie <nathanialkrauss@gmail.com>
2023-03-21 20:25:18 -0400

    updated usb trigger script + tested

--- basic/usb_trigger.py
+++ basic/usb_trigger.py
@@ -1,57 +1,77 @@
 #!/usr/bin/env -S python3 -u
 import os
 import time
+import datetime
 
+# logging
+log_file = "trigger_log.txt"
+f = open(log_file, "w")
 
-def plugin():
-    """
-    Checks every 0.5 seconds for two storage devices to be plugged in
-    :return:
-    """
-    while True:
-        # check for newly mounted devices
-        stream = os.popen('lsblk')
-        output: list[str] = stream.readlines()
+def log(msg : str):
+    text = '{} >> {}'.format(datetime.datetime.now(), msg)
+    f.write(text)
+    print(text)
 
-        # maintain a count of partitions
-        sda_count = 0
-        sdb_count = 0
+def check_usbs():
+    '''
+    Checks for any USB connections, returns False when two USBs are found, otherwise returns True.
+    '''
+    stream = os.popen('lsblk')
+    output : str = stream.read()
+    if 'sda' in output and 'sdb' in output:
+        log("FOUND 2 USB storage devices!")
+        return False 
+    elif 'sda' in output and 'sdb' not in output:
+        log('found first USB storage device, waiting on second')
+        return True
+    elif 'sda' not in output and 'sdb' in output:
+        log('this is a weird error...this was entirely unexpected')
+        return True
+    else:        
+        log("didn't find two USB storage devices")
+        return True
 
-        # check for 1st and 2nd mounted devices
-        for line in output:
-            if 'sda' in line:
-                sda_count += 1
-            elif 'sdb' in line:
-                sdb_count += 1
+def reset():
+    if os.path.exists('/dev/sda*'):
+        os.system('sudo rm -rf /dev/sda*')
+    if os.path.exists('/dev/sdb*'):
+        os.system('sudo rm -rf /dev/sdb*')
 
-        if sda_count > 0 and sdb_count > 0:
-            paths = []
-            # sda1 --> "dirty USB" (with malicious files)
-            # sdb1 --> "clean USB" (with zero files) that we copy files from sda1 from
-            # for i in (sda_count - 1):
-            #     paths.append('/dev/sda{}'.format(i + 1))
-            # for i in (sdb_count - 1):
-            #     paths.append('/dev/sdb{}'.format(i + 1))
+def main():
+    reset()
 
-            exec("/home/usb-border-patrol/usb-border-patrol/basic/unpack.py", "/dev/sda1")
-            exec("/home/usb-border-patrol/usb-border-patrol/basic/av_script.py", '/dev/sda1')
-
-            # prevent future temporarily, will need to change or final script needs to restart script
-            unplug()
-
+    # wait for 2 USBs to be connected
+    while check_usbs():
         time.sleep(0.5)
 
+    # loop exited, two USBs have been detected
+    log("loop exited, continuing with trigger script...")
+    
+    # check if mount-points already exist
+    if os.path.exists("/media/untrusted"):
+        log("/media/untrusted already exists, deleting folder")
+        os.system('sudo umount /media/untrusted')
+        os.system('sudo rm -rf /media/untrusted')
+    if os.path.exists("/media/trusted"):
+        log("/media/trusted already exists, deleting folder")
+        os.system('sudo umount /media/trusted')
+        os.system('sudo rm -rf /media/trusted')
 
-def unplug():
-    """
-    Checks for when all devices are unplugged
-    :return:
-    """
-    while True:
-        stream = os.popen('lsblk')
-        output: str = stream.read()
+    # create directories for mount points
+    os.system('sudo mkdir /media/untrusted')
+    os.system('sudo mkdir /media/trusted')
 
-        if 'sda' not in output and 'sdb' not in output:
-            break
+    # Change mount-points of USB to appropriate paths
+    # UNTRUSTED USB: "/media/untrusted"
+    # TRUSTED USB:   "/media/trusted"
+    log("attempting to mount USB storage devices to pre-defined folders")
+    os.system('sudo mount /dev/sda1 /media/untrusted')
+    os.system('sudo mount /dev/sdb1 /media/trusted')
 
-        time.sleep(0.5)
+    # execute scripts
+    log("USB storage devices have been mounted, now executing other USB Border Patrol Scripts")
+    #os.system('/home/usb-border-patrol/usb-border-patrol/basic/unpack.py /media/untrusted /media/trusted')
+    #os.system('/home/usb-border-patrol/usb-border-patrol/basic/av_script.py /media/untrusted /media/trusted')
+
+if __name__ == "__main__":
+    main()
