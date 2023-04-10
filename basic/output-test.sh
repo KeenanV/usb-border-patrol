24bcf8d73313d4065e6281e0cce427a4c2549807
KeenanV <keenanvandeusen@gmail.com>
2023-04-10 18:18:08 -0400

    updates

--- basic/output-test.sh
+++ basic/output-test.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
 width=78
-height=44
+height=$(tput lines)
 v_center=$((height / 2 - 8))
 
-fonts_dir=/home/keenan/Documents/capstone/figlet-fonts
+fonts_dir=/home/usbbp/figlet-fonts
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
 
-tmux new-session -d -x $width -y $height
-
-commands='clear'
+clear
 
 for ii in $(seq 0 $v_center); do
-    commands="$commands; echo"
+    echo
 done
 
 if [ $# -eq 1 ]; then
     if [ "$1" = "id" ]; then
-        commands="$commands; echo \"${YELLOW}\""
-        commands="$commands; figlet \"INPUT DEV.\" -d $fonts_dir -f \"ANSI Shadow\" -t -c"
-        commands="$commands; echo \"${GREEN}\""
-        commands="$commands; figlet \"BLOCKED\" -d $fonts_dir -f \"ANSI Shadow\" -t -c"
+        echo "${YELLOW}"
+        figlet "INPUT DEV." -d $fonts_dir -f "ANSI Shadow" -t -c
+        echo "${GREEN}"
+        figlet "BLOCKED" -d $fonts_dir -f "ANSI Shadow" -t -c
+
+		for ii in $(seq 0 $((v_center - 4))); do
+			echo
+		done
     fi
 else
     ff="/home/usbbp/tmp/gb-tmp.txt"
@@ -37,21 +39,18 @@ else
     bad=$(($bad1 + $bad2))
 
     echo "${GREEN}"
-    figlet "GOOD: ${good}" -d //usbbp/figlet-fonts -f "ANSI Shadow" -t -c
+    figlet "GOOD: ${good}" -d $fonts_dir -f "ANSI Shadow" -t -c
 
     echo "${RED}"
-    figlet "BAD: ${bad}" -d /home/usbbp/figlet-fonts -f "ANSI Shadow" -t -c
+    figlet "BAD: ${bad}" -d $fonts_dir -f "ANSI Shadow" -t -c
 
     echo "${YELLOW}"
-    figlet "SUS: 0" -d /home/usbbp/figlet-fonts -f "ANSI Shadow" -t -c
+    figlet "SUS: 0" -d $fonts_dir -f "ANSI Shadow" -t -c
+	
+	for ii in $(seq 0 $((v_center - 7))); do
+		echo
+	done
 fi
 
 echo "${NC}"
-
-for ii in $(seq 0 $((v_center - 8))); do
-    commands="$commands; echo"
-done
-
-tmux send-keys "$commands" C-m
-tmux attach
 
